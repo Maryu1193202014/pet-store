23a5065707869c6b93ddce143cd522791f1487e2
Maryuri <maryuortiz25@gmail.com>
2025-03-27 20:09:05 -0500

    Register user request

---
+++ src/config/database.php
@@ -0,0 +1,23 @@
+<?php 
+
+$host = "localhost";
+$port ="5432";
+$dbname= "petstore";
+$user = "postgres";
+$password="unicesmag";
+
+$data_connection="
+   host=$host
+   port=$port
+   dbname=$dbname
+   user=$user
+   password=$password
+   ";
+$conn = pg_connect($data_connection);
+if(!$conn){
+    echo"conection error";
+}else{
+echo"success !!!";
+}
+//pg_close($conn)
+?>
